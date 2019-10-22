ff64a085d9e79af641cd3366a82ff8f195c445e3
sriyuthsagi <42911878+sriyuthsagi@users.noreply.github.com>
2019-10-22 18:24:51 -0400

    Update price_identify.py

--- price_identify.py
+++ price_identify.py
@@ -11,34 +11,47 @@ follows a rule of the price being followed by the currency or preceded by $
 sentence = 'I can sell the coffee for $4.54'
 
 price = 0
-
-if 'yuan' in sentence:
-    curr_start = sentence.find('yuan') - 1
+def priceIdentifty(sentence):
     
-    temp = curr_start - 1
+    currencies = ['dollar', 'yuan', 'kuai', 'RMB']
+    curr = 'None'
     
+    for i in currencies:
+        if i in sentence:
+            curr = i
     
-    while (sentence[temp] != ' '):
-        temp = temp - 1
-        if temp == -1:
-            break
+    if curr != 'None':
+        curr_start = sentence.find(curr) - 1
         
-    temp = temp + 1
-    
-    price = float(sentence[temp : curr_start])
-
-
-if '$' in sentence:
-    curr_start = sentence.find('$') + 1
-    
-    temp = curr_start
-    
-    while (sentence[temp] != ' '):
+        temp = curr_start - 1
+        
+        
+        while (sentence[temp] != ' '):
+            temp = temp - 1
+            if temp == -1:
+                break
+            
         temp = temp + 1
-        if temp == len(sentence):
-            break
+        
+        price = float(sentence[temp : curr_start])
     
-    price = float(sentence[curr_start : temp])
+    
+    if '$' in sentence:
+        curr_start = sentence.find('$') + 1
+        
+        temp = curr_start
+        
+        while (sentence[temp] != ' '):
+            temp = temp + 1
+            if temp == len(sentence):
+                break
+        
+        price = float(sentence[curr_start : temp])
 
-    
-print(price)
+    return price
+
+
+
+
+
+print(priceIdentifty(sentence))
